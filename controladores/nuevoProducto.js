b477672402d1e244d59450d2983ab01fb5f8851b
Mitsukai74 <ansaanva@gmail.com>
2022-07-18 18:08:12 -0500

    Corrección de errores V2

--- controladores/nuevoProducto.js
+++ controladores/nuevoProducto.js
@@ -4,9 +4,10 @@ const formulario=document.querySelector("[data-form]");
 
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault();
+    const imagen=document.querySelector("[data-imagen]").value;
     const nombre=document.querySelector("[data-nombre]").value;
-    const descipt=document.querySelector("[data-descrip]").value;
-    console.log("formulario");
+    const descript=document.querySelector("[data-descrip]").value;
+    console.log(imagen,"-" ,nombre, "-", descript);
     productServices.crearProducto(imagen,nombre,descrip).then( respuesta => {
         console.log(respuesta);
     }).catch(err => console.log( err));
