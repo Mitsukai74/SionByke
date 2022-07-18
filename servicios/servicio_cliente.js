923ee45829eaee11cd980e239c1417e660dbbe98
Mitsukai74 <ansaanva@gmail.com>
2022-07-18 16:44:49 -0500

    intentando crear producto v1,

--- servicios/servicio_cliente.js
+++ servicios/servicio_cliente.js
@@ -1,3 +1,17 @@
+const crearnuevoProducto = (imagen,nombre,descipcion) => {
+const linea=document.createElement("div");
+const contenido =    
+        `<img alt="casco Scott" class="casco__imagen" src="${imagen}"/>
+        <div class="casco__contenido">
+            <h4 class="casco__titulo">${nombre}</h4>
+            <p class="casco__descripcion">${descipcion}</p>
+            <button class="casco__boton">Agregar al carrito</button>
+        </div>`;
+        linea.innerHTML=contenido;
+        return linea;
+};
+
+
 //para hacer la conexión entre el back y el front
 const crearProducto=(img,nombre,descrip) => {
     return fetch("http://localhost:3000/producto", {
